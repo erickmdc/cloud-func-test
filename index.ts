571db0e56169e0abd71a0474c1b575b11a748861
erickmdc <emc.7@hotmail.com>
2025-04-29 17:01:52 -0300

    sdfsdfs

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import path from "path";
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(express.static(path.join(__dirname, "public"))); // Serve static files
 
